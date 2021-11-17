cf67ab39375da6f75fadac478aa50ba4248c5aee
Nick Jones <nick@space48.com>
2021-11-17 15:18:32 +0000

    Use correct price field

--- src/Components/UI/Basic/Item/Price.tsx
+++ src/Components/UI/Basic/Item/Price.tsx
@@ -3,7 +3,7 @@ import { ItemHandlerProps } from "../../../Data/Item";
 const Price = (props: ItemHandlerProps) => {
   return (
     <div className="xo__card-product__price">
-      &pound;{props.item.product.price_GBP}
+      &pound;{props.item.product.price}
     </div>
   );
 };
