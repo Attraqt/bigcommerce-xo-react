0ec227e310ffcfcea1353f9b4ffa18d6641d85fe
Nick Jones <nick@space48.com>
2021-12-02 16:10:46 +0000

    Add lodash specific eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,26 +4,25 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:security/recommended',
-    'prettier'
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:security/recommended",
+    "prettier",
+    "plugin:lodash/recommended",
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: [
-    'react',
-    'security',
-    '@typescript-eslint',
-    'prettier'
-  ],
+  plugins: ["react", "security", "@typescript-eslint", "prettier", "lodash"],
   rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.tsx'] }],
+    "react/jsx-filename-extension": [
+      1,
+      { extensions: [".js", ".jsx", ".tsx"] },
+    ],
   },
 };
