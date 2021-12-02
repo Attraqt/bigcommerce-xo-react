dc31df23c0312f8d3b5ce6322080ddfac2b1cd22
Nick Jones <nick@space48.com>
2021-12-02 16:13:12 +0000

    Drive facet rendering from BigCommerce configuration, rather than the API response
    - This allows us to implement filter facets, which aren't in the API response

--- src/Components/UI/Cornerstone/FacetContainer.tsx
+++ src/Components/UI/Cornerstone/FacetContainer.tsx
@@ -1,32 +1,34 @@
 import {
-  Facet,
   FacetContainerProps,
   FacetController,
+  FacetProps,
   FacetResolver,
+  FilterSpecification,
+  FilterController,
+  Filter,
 } from "../../Data/Facet";
-import { find } from "lodash";
 import FacetBasic from "./Facets/FacetBasic";
 import {
   BigCommerceConfigurationProps,
   BigCommerceXOConfig,
 } from "../../Data/WithBigCommerceConfiguration";
 import FacetDataList from "../Basic/Facet/FacetDataList";
+import FilterMinMax from "../Basic/Filter/FilterMinMax";
+import { filter } from "lodash";
 
 const facetResolverFactory = (config: BigCommerceXOConfig): FacetResolver => {
-  return (f: Facet) => {
-    const facetConfig = config.customFacetConfigurations?.find(
-      (a) => a.attribute === f.id.replace(/^facet-/, "")
-    );
-
-    if (!facetConfig) return;
-
-    switch (facetConfig.ui) {
-      case "basic": {
-        return FacetBasic;
+  return (ui: string, isFilter: boolean) => {
+    if (!isFilter) {
+      switch (ui) {
+        case "basic": {
+          return FacetBasic;
+        }
+        case "datalist": {
+          return FacetDataList;
+        }
       }
-      case "datalist": {
-        return FacetDataList;
-      }
+    } else {
+      return FilterMinMax;
     }
   };
 };
@@ -34,46 +36,63 @@ const facetResolverFactory = (config: BigCommerceXOConfig): FacetResolver => {
 const FacetContainer = (
   props: FacetContainerProps & BigCommerceConfigurationProps
 ) => {
-  const controller = new FacetController(props);
+  const facetController = new FacetController(props);
+  const filterController = new FilterController(props);
+
   const componentResolver: FacetResolver =
     props.facetComponentResolver ||
     facetResolverFactory(props?.bigCommerceConfig || {});
+  const facets = props.bigCommerceConfig.customFacetConfigurations?.map(
+    (customFacet, index) => {
+      const isFilter = customFacet.isFilter;
+      const facet = props.available.find((f) => f.id === customFacet.attribute);
+      const isInResponse = facet !== undefined;
 
-  const getFacetConfig = (attribute: string) =>
-    props.bigCommerceConfig.customFacetConfigurations?.find(
-      (a) => a.attribute === attribute.replace(/^facet-/, "")
-    );
+      if (!isFilter && !isInResponse) return;
 
-  const facets = props.available.map((facet, index) => {
-    if (!facet) return;
+      const FacetComponent = componentResolver(
+        customFacet.ui,
+        customFacet.isFilter
+      );
 
-    const config = getFacetConfig(facet.id);
+      if (!FacetComponent) return;
 
-    if (!config) return;
+      const componentProps: FacetProps = {
+        title: facet?.title || "Undefined Title",
+        id: facet?.id || customFacet.attribute,
+        isFilter,
+        isLoading: props.isLoading,
+      };
 
-    const selectedFacet = find(props.active, (f) => f?.id == facet.id);
-    const FacetComponent = componentResolver(facet);
+      if (isFilter) {
+        componentProps.selectedFilter =
+          props.filter.find((f) => f.id === customFacet.attribute)?.filter ??
+          "";
+        componentProps.updateSelectedFilter = (value: FilterSpecification) => {
+          filterController.updateSelected(customFacet.attribute, value);
+        };
+      } else {
+        componentProps.availableValues = facet?.values || [];
+        componentProps.selectedValues =
+          props.active.find((a) => a.id === customFacet.attribute)?.values ||
+          [];
+        componentProps.toggleSelectedValue = (value: string) => {
+          facetController.updateSelected(facet!, value);
+        };
+      }
 
-    if (!FacetComponent) return;
-
-    return (
-      <div
-        id="facetedSearch-navList"
-        className="facetedSearch-navList"
-        key={index}
-      >
-        <div className="accordion accordion--navList"></div>
-        <FacetComponent
-          facet={facet}
-          selectedValues={selectedFacet ? selectedFacet.values : []}
-          toggleSelectedValue={(value: string) => {
-            controller.updateSelected(facet, value);
-          }}
-          isLoading={props.isLoading}
-        />
-      </div>
-    );
-  });
+      return (
+        <div
+          id="facetedSearch-navList"
+          className="facetedSearch-navList"
+          key={index}
+        >
+          <div className="accordion accordion--navList"></div>
+          <FacetComponent {...componentProps} />
+        </div>
+      );
+    }
+  );
 
   return (
     <div id="facetedSearch" className="facetedSearch sidebarBlock">
